3ccf664801c67c3e4abd6e71c440ce7894d5ca60
Rakibul-Hasan-max <rakibulhasan4041@gmail.com>
2022-12-15 00:16:56 +0600

    footer design1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import About from './Pages/About/About';
+import Footer from './Pages/Components/Footer/Footer';
+import Dashboard from './Pages/Dashboard/Dashboard';
 import Home from './Pages/Home/Home';
 import NotFound from './Pages/NotFound/NotFound';
 
@@ -12,8 +14,10 @@ function App() {
           <Route path="/" element={<Home />}/>
           <Route path="/home" element={<Home />} />
           <Route path="/about" element={<About />} />
+          <Route path="/dashboard" element={<Dashboard />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
+        <Footer/>
       </BrowserRouter>
     </div>
   );
---
+++ src/Pages/Components/Footer/Footer.js
@@ -0,0 +1,145 @@
+import { Button, Container, Grid, Typography } from "@mui/material";
+import { Box } from "@mui/system";
+import { FaFacebookF, FaInstagram, FaTwitter, FaYoutube } from "react-icons/fa";
+import { NavLink, useLocation } from "react-router-dom";
+import logo from "../../../images/logo.png";
+import "./Footer.css";
+
+const Footer = () => {
+  const location = useLocation();
+  if (!location?.pathname?.includes("dashboard"))
+    return (
+      <Box
+        className="footer"
+        sx={{
+          backgroundColor: "#0D0D0D",
+          border: "1px solid black",
+          overflow: "hidden",
+          color: "inherit",
+        }}
+      >
+        <Container maxWidth="lg">
+          <Grid container spacing={5}>
+            <Grid item xs={12} md={3}>
+              <Box>
+                <img
+                  style={{ width: "162.5px", height: "auto" }}
+                  src={logo}
+                  alt=""
+                />
+                <Box sx={{ my: 2 }}>
+                  <Typography variant="subtitle2" sx={{ color: "#fff", my: 1 }}>
+                    Make your life easier with us
+                  </Typography>
+                  <Box sx={{ display: "flex", mt: 2, color: "#fff" }}>
+                    <Box className="footer-icon">
+                      <FaFacebookF />
+                    </Box>
+                    <Box className="footer-icon">
+                      <FaInstagram />
+                    </Box>
+                    <Box className="footer-icon">
+                      <FaTwitter />
+                    </Box>
+                    <Box className="footer-icon">
+                      <FaYoutube />
+                    </Box>
+                  </Box>
+                </Box>
+              </Box>
+            </Grid>
+            <Grid item xs={12} md={5} sx={{ color: "#fff" }}>
+              <Typography variant="h6" sx={{ fontSize: 25 }}>
+                About Us
+              </Typography>
+              <Typography variant="body2" sx={{ my: 3 }}>
+                Here we are ready to provide you with the best quality product
+                from our own production. If you need any product, feel free to
+                inbox us with the product code and place your order.
+              </Typography>
+              <NavLink to="/about" style={{ textDecoration: "none" }}>
+                <Button
+                  variant="contained"
+                  sx={{
+                    background: "#f42828 !important",
+                    borderRadius: "0 !important",
+                    py: 2,
+                    px: 5,
+                  }}
+                >
+                  More About Us
+                </Button>
+              </NavLink>
+            </Grid>
+            <Grid item xs={12} md={4} sx={{ color: "#fff" }}>
+              <Grid container spacing={3}>
+                <Grid item xs={12} sm={6}>
+                  <Box>
+                    <Typography variant="h6" sx={{ fontSize: 25 }}>
+                      Careers
+                    </Typography>
+                    <ul style={{ marginTop: "24px" }}>
+                      <li>
+                        <Typography variant="caption">
+                          Content Writer
+                        </Typography>
+                      </li>
+                      <li>
+                        <Typography variant="caption">Video Editor</Typography>
+                      </li>
+                      <li>
+                        <Typography variant="caption">
+                          Digital Marketer
+                        </Typography>
+                      </li>
+                      <li>
+                        <Typography variant="caption">
+                          Graphic Designer
+                        </Typography>
+                      </li>
+                      <li>
+                        <Typography variant="caption">It Expert</Typography>
+                      </li>
+                    </ul>
+                  </Box>
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <Box>
+                    <Typography variant="h6" sx={{ fontSize: 25 }}>
+                      Our Service
+                    </Typography>
+                    <ul style={{ marginTop: "24px" }}>
+                      <li>
+                        <Typography variant="caption">E-Commerce</Typography>
+                      </li>
+                      <li>
+                        <Typography variant="caption">Production</Typography>
+                      </li>
+                      <li>
+                        <Typography variant="caption">Home Delivery</Typography>
+                      </li>
+                      <li>
+                        <Typography variant="caption">
+                          Company Outlet
+                        </Typography>
+                      </li>
+                      <li>
+                        <Typography variant="caption">IT Service</Typography>
+                      </li>
+                    </ul>
+                  </Box>
+                </Grid>
+              </Grid>
+            </Grid>
+          </Grid>
+          <Box sx={{ textAlign: "left" }}>
+            <Typography variant="caption" sx={{ color: "#fff" }}>
+              Copyright © 2023 Matricolive, All rights reserved.
+            </Typography>
+          </Box>
+        </Container>
+      </Box>
+    );
+};
+
+export default Footer;
