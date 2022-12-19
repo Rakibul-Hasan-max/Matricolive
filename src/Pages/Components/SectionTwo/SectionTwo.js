58b89af93c102661c1216db0ba048b3a20ee9a5a
Rakibul-Hasan-max <rakibulhasan4041@gmail.com>
2022-12-19 14:53:06 +0600

    sectionTwoStart

---
+++ src/Pages/Components/SectionTwo/SectionTwo.js
@@ -0,0 +1,27 @@
+import { Box, Grid, Typography } from "@mui/material";
+import { Container } from "@mui/system";
+import FProduct from "./FProduct";
+
+const SectionTwo = () => {
+  return (
+    <Box>
+      <Container maxWidth="lg">
+        <Grid>
+          <Box>
+            <Typography
+              variant="h5"
+              sx={{ textAlign: "left", fontWeight: 500, mx: 5, mt: 5 }}
+            >
+              Featured Product <hr/>
+            </Typography>
+            <Box>
+                <FProduct />
+            </Box>
+          </Box>
+        </Grid>
+      </Container>
+    </Box>
+  );
+};
+
+export default SectionTwo;
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,9 +1,13 @@
+import Banner from "../Components/Banner/Banner";
 import SectionOne from "../Components/SectionOne/SectionOne";
+import SectionTwo from "../Components/SectionTwo/SectionTwo";
 
 const Home = () => {
   return (
     <div>
+      <Banner />
       <SectionOne />
+      <SectionTwo />
     </div>
   );
 };
