e76c8d2245ced54f0d83b127f06a8dc96929367b
Rakibul-Hasan-max <rakibulhasan4041@gmail.com>
2022-12-22 22:30:55 +0600

    sectiononedone

--- src/Pages/Components/SectionOne/SectionOne.js
+++ src/Pages/Components/SectionOne/SectionOne.js
@@ -1,3 +1,8 @@
+import LocalShippingIcon from "@mui/icons-material/LocalShipping";
+import ManageHistoryIcon from "@mui/icons-material/ManageHistory";
+import PaymentIcon from "@mui/icons-material/Payment";
+import PermPhoneMsgIcon from "@mui/icons-material/PermPhoneMsg";
+import RedeemIcon from "@mui/icons-material/Redeem";
 import { Typography } from "@mui/material";
 import { Box } from "@mui/system";
 
@@ -8,64 +13,99 @@ const SectionOne = () => {
         display: "flex",
         flexWrap: "wrap",
         border: "1px solid #E5B8F4",
-        padding: "25px 50px",
+        padding: "25px 0px",
         margin: "20px 70px 60px 70px",
         justifyContent: "center",
       }}
     >
       <Box
         sx={{
-          paddingRight: "30px",
+          paddingRight: "20px",
           borderRight: "1px solid #E5B8F4",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          gap: "10px",
         }}
       >
-        <Typography variant="body1">Free Delivery</Typography>
-        <Typography variant="caption" sx={{ color: "grey" }}>
-          For all orders over ৳5000
-        </Typography>
+        <LocalShippingIcon color="success" fontSize="large" />
+        <Box>
+          <Typography variant="body1">Free Delivery</Typography>
+          <Typography variant="caption" sx={{ color: "grey" }}>
+            For all orders over ৳5000
+          </Typography>
+        </Box>
       </Box>
       <Box
         sx={{
-          padding: "0 30px",
+          padding: "0 20px",
           borderRight: "1px solid #E5B8F4",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          gap: "10px",
         }}
       >
-        <Typography variant="body1">90 Days Return</Typography>
-        <Typography variant="caption" sx={{ color: "grey" }}>
-          If goods have problems
-        </Typography>
+        <ManageHistoryIcon color="success" fontSize="large" />
+        <Box>
+          <Typography variant="body1">90 Days Return</Typography>
+          <Typography variant="caption" sx={{ color: "grey" }}>
+            If goods have problems
+          </Typography>
+        </Box>
       </Box>
       <Box
         sx={{
-          padding: "0 30px",
+          padding: "0 20px",
           borderRight: "1px solid #E5B8F4",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          gap: "10px",
         }}
       >
-        <Typography variant="body1">Secure Payment</Typography>
-        <Typography variant="caption" sx={{ color: "grey" }}>
-          100% secure payment
-        </Typography>
+        <PaymentIcon color="success" fontSize="large" />
+        <Box>
+          <Typography variant="body1">Secure Payment</Typography>
+          <Typography variant="caption" sx={{ color: "grey" }}>
+            100% secure payment
+          </Typography>
+        </Box>
       </Box>
       <Box
         sx={{
-          padding: "0 30px",
+          padding: "0 20px",
           borderRight: "1px solid #E5B8F4",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          gap: "10px",
         }}
       >
-        <Typography variant="body1">24/7 Support</Typography>
-        <Typography variant="caption" sx={{ color: "grey" }}>
-          Dedicated support
-        </Typography>
+        <PermPhoneMsgIcon color="success" fontSize="large" />
+        <Box>
+          <Typography variant="body1">24/7 Support</Typography>
+          <Typography variant="caption" sx={{ color: "grey" }}>
+            Dedicated support
+          </Typography>
+        </Box>
       </Box>
       <Box
         sx={{
-          paddingLeft: "30px",
+          paddingLeft: "20px",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          gap: "10px",
         }}
       >
-        <Typography variant="body1">Gift Service</Typography>
-        <Typography variant="caption" sx={{ color: "grey" }}>
-          Support gift service
-        </Typography>
+        <RedeemIcon color="success" fontSize="large" />
+        <Box>
+          <Typography variant="body1">Gift Service</Typography>
+          <Typography variant="caption" sx={{ color: "grey" }}>
+            Support gift service
+          </Typography>
+        </Box>
       </Box>
     </Box>
   );
