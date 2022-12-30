b9388f285dfe80b0cd4b4b3df0b52ebc43600bf5
Rakibul-Hasan-max <rakibulhasan4041@gmail.com>
2022-12-31 00:09:31 +0600

    details add2

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -2,7 +2,7 @@
 const Dashboard = () => {
     return (
         <div>
-            This is dashboard ss
+            <img src="https://images.pexels.com/photos/4348289/pexels-photo-4348289.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
         </div>
     );
 };
