61888ff6eb611a1e353583e2fd17ba742503ac8d
Rakibul-Hasan-max <rakibulhasan4041@gmail.com>
2023-01-07 21:45:26 +0600

    day 21

--- src/Pages/Components/AddBanner/AddBanner.js
+++ src/Pages/Components/AddBanner/AddBanner.js
@@ -2,7 +2,7 @@
 const AddBanner = () => {
     return (
         <div>
-            <img style={{ width: "88.5%", maxHeight: "380px", margin: "30px 0" }} src="https://i.ibb.co/TM5mr0s/banner4.png" alt="Product Banner" />
+            <img style={{ width: "100%", maxHeight: "380px", margin: "30px 0" }} src="https://i.ibb.co/d2W4Vd8/Social-banner2.png" alt="Product Banner" />
         </div>
     );
 };
