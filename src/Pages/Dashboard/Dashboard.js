12017ff2b7426dd1e6c63f78b698f0dfac72aec1
Rakibul-Hasan-max <rakibulhasan4041@gmail.com>
2023-01-19 22:37:56 +0600

    about update4

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -2,7 +2,7 @@
 const Dashboard = () => {
     return (
         <div>
-            <img src="https://images.pexels.com/photos/4348289/pexels-photo-4348289.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+            Dashboard Coming Soon...
         </div>
     );
 };
