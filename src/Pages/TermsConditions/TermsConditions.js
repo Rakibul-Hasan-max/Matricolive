8db552b4a8f9e0ec1389f6e177202481a049cc2e
Rakibul-Hasan-max <rakibulhasan4041@gmail.com>
2023-02-06 15:42:09 +0600

    terms add

--- src/Pages/TermsConditions/TermsConditions.js
+++ src/Pages/TermsConditions/TermsConditions.js
@@ -1,14 +1,90 @@
-import { Typography } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 
 const TermsConditions = () => {
-    return (
-        <div>
-            Terms & Conditions page coming soon...
-            <Typography>
-                
-            </Typography>
-        </div>
-    );
+  return (
+    <Box>
+      <Typography
+        variant="h5"
+        sx={{ textAlign: "left", mt: 5, mx: "10%", fontWeight: 500 }}
+      >
+        Terms and Conditions
+      </Typography>
+      <Typography
+        variant="body1"
+        sx={{ textAlign: "left", mt: 2, mb: 3, mx: "10%", fontWeight: 500 }}
+      >
+        INTRODUCTION
+      </Typography>
+      <Typography variant="body2" sx={{ textAlign: "left", mb: 2, mx: "10%" }}>
+        Welcome to Matricolive Terms and Conditions page. Please read the
+        following terms and conditions carefully as it constitutes a contract
+        for your purchase and sale of goods with us. These Terms constitute the
+        entire agreement between the parties to the exclusion of all prior
+        discussions, representations, promotional materials, web-site content,
+        documents, correspondence, or any other terms. You acknowledge that you
+        have not relied on any condition, representation, condition or warranty,
+        express or implied by statute, common law or otherwise, other than as
+        expressly set out in these Terms. Terms only bring the buyer and seller
+        to a proper settlement. Which defines a proper boundary between us and
+        you.
+      </Typography>
+      <Typography variant="body2" sx={{ textAlign: "left", mb: 2, mx: "10%" }}>
+        These Terms indicate your consent to our Privacy Policy (Privacy
+        Statement). and agree to buy and sell products with us in accordance
+        with our privacy policy. Subject to the terms and conditions as per the
+        Consumer Act 2009.
+      </Typography>
+      <Typography variant="body2" sx={{ textAlign: "left", mb: 2, mx: "10%" }}>
+        Term staff are not authorized to change the terms and conditions under
+        any circumstances. Any variation to these terms and conditions must be
+        agreed to in writing and signed by the director of Term. Additional
+        terms may apply to certain products and services that the Term provides
+        to you.
+      </Typography>
+      <Typography
+        variant="body1"
+        sx={{ textAlign: "left", mt: 5, mb: 3, mx: "10%", fontWeight: 500 }}
+      >
+        Access and Use of Registration
+      </Typography>
+      <Typography variant="body2" sx={{ textAlign: "left", mb: 2, mx: "10%" }}>
+        We inform you that you must register before using this site, after which
+        you will be granted permission to use it. We welcome users to register
+        on our digital platforms. We offer the following registration services
+        which may change in the future All changes will be attached to the Terms
+        and Conditions page and communicated to existing users via email.
+      </Typography>
+      <Typography variant="body2" sx={{ textAlign: "left", mb: 2, mx: "10%" }}>
+        Registration services are offered only to individual customers. Please
+        contact or write to us if multiple people offer access to the same
+        account or for corporate accounts. Subscription rates will vary for
+        multiple simultaneous accesses. <br />
+        - It is completely free of usage charges <br />
+        - It does not charge any fee for vendor registration. <br />
+        - It always provides you free customer service. <br />
+        - It allows use by 14 to 90 year olds. <br />
+        - There is no charge for using it <br />- It is open to all.
+      </Typography>
+      <Typography variant="body2" sx={{ textAlign: "left", mb: 2, mx: "10%" }}>
+        On this site the customer will register to enjoy all the benefits
+        reserved for the customer. Sellers must follow some of our internal
+        rules to register and thereafter they will enjoy all seller benefits. A
+        person can register once. Having multiple accounts will be permanently
+        restricted. We allow one person to use it after registration. Please
+        read the terms of use to use it. <br />
+        - It does not recommend the use of any spam <br />
+        - It does not prioritize any terrorist activities. <br />
+        - It does not mandate the sale of any illegal goods. <br />
+        - It does not allow anti-national or anti-social behavior. <br />- It
+        does not encourage any illegal activity.
+      </Typography>
+      <Typography variant="body2" sx={{ textAlign: "left", mb: 2, mx: "10%" }}>
+        Metricolive always works for your good. So it always protects you from
+        all types of illegal activities. Your contribution is an asset to us, so
+        we promise to always keep you safe from the activities of spammers.
+      </Typography>
+    </Box>
+  );
 };
 
 export default TermsConditions;
