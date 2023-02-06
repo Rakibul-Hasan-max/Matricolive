887fd76532540f6c7b6c7acdc289d4fc8aabee14
Rakibul-Hasan-max <rakibulhasan4041@gmail.com>
2023-02-06 17:07:55 +0600

    contact page design

--- src/Pages/ContactUs/ContactUs.js
+++ src/Pages/ContactUs/ContactUs.js
@@ -1,10 +1,47 @@
+import { Box, Button, TextField, Typography } from "@mui/material";
 
 const ContactUs = () => {
-    return (
-        <div>
-            Contact Us
-        </div>
-    );
+  return (
+    <Box>
+      <Typography variant="h5" sx={{ mt: "80px", fontWeight: 500 }}>
+        Get in Touch
+      </Typography>
+      <Box>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            gap: "20px",
+            mx: "15%",
+            mt: 5,
+          }}
+        >
+          <TextField id="filled-basic" label="Name" variant="filled" />
+          <TextField id="filled-basic" label="Email" variant="filled" />
+          <TextField
+            id="filled-textarea"
+            label="Message"
+            multiline
+            variant="filled"
+          />
+        </Box>
+        <Button
+          variant="contained"
+          sx={{
+            mt: 5,
+            mb: 3,
+            background: "#336699 !important",
+            borderRadius: "0 !important",
+            py: 1,
+            px: 3,
+          }}
+        >
+          Send Message
+        </Button>
+        <Box></Box>
+      </Box>
+    </Box>
+  );
 };
 
 export default ContactUs;
