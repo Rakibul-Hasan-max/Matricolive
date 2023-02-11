2214f43b62206b93c078ab597310c25af2fc7a9e
Rakibul-Hasan-max <rakibulhasan4041@gmail.com>
2023-02-11 12:50:50 +0600

    bottom nav2

--- src/Pages/Components/Shared/BottomNav/BottomNav.js
+++ src/Pages/Components/Shared/BottomNav/BottomNav.js
@@ -1,41 +1,42 @@
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import FolderIcon from '@mui/icons-material/Folder';
-import LocationOnIcon from '@mui/icons-material/LocationOn';
-import RestoreIcon from '@mui/icons-material/Restore';
-import BottomNavigation from '@mui/material/BottomNavigation';
-import BottomNavigationAction from '@mui/material/BottomNavigationAction';
-import * as React from 'react';
+import CategoryIcon from "@mui/icons-material/Category";
+import HomeIcon from "@mui/icons-material/Home";
+import PersonIcon from "@mui/icons-material/Person";
+import ShopIcon from "@mui/icons-material/Shop";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import BottomNavigation from "@mui/material/BottomNavigation";
+import BottomNavigationAction from "@mui/material/BottomNavigationAction";
+import * as React from "react";
 
 export default function BottomNav() {
-  const [value, setValue] = React.useState('home');
+  const [value, setValue] = React.useState("home");
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
 
   return (
-    <BottomNavigation sx={{ width: "100%", display: { xs: "flex", sm: "none" } }} value={value} onChange={handleChange}>
-      <BottomNavigationAction
-        label="Home"
-        value="home"
-        icon={<RestoreIcon />}
-      />
-      <BottomNavigationAction
-        label="Shop"
-        value="shop"
-        icon={<FavoriteIcon />}
-      />
+    <BottomNavigation
+      sx={{ width: "100%", display: { xs: "flex", sm: "none" } }}
+      value={value}
+      onChange={handleChange}
+    >
+      <BottomNavigationAction label="Home" value="home" icon={<HomeIcon />} />
+      <BottomNavigationAction label="Shop" value="shop" icon={<ShopIcon />} />
       <BottomNavigationAction
         label="Category"
         value="category"
-        icon={<LocationOnIcon />}
+        icon={<CategoryIcon />}
       />
       <BottomNavigationAction
         label="Cart"
         value="cart"
-        icon={<LocationOnIcon />}
+        icon={<ShoppingCartIcon />}
       />
-      <BottomNavigationAction label="Account" value="account" icon={<FolderIcon />} />
+      <BottomNavigationAction
+        label="Account"
+        value="account"
+        icon={<PersonIcon />}
+      />
     </BottomNavigation>
   );
 }
