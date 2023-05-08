4adf8c559e8b42b572f512e6d267f7d62cb7edcf
Rakibul-Hasan-max <rakibulhasan4041@gmail.com>
2023-05-08 22:59:57 +0600

    terms done

--- src/Pages/TermsConditions/TermsConditions.js
+++ src/Pages/TermsConditions/TermsConditions.js
@@ -83,6 +83,96 @@ const TermsConditions = () => {
         all types of illegal activities. Your contribution is an asset to us, so
         we promise to always keep you safe from the activities of spammers.
       </Typography>
+      <Typography
+        variant="body1"
+        sx={{ textAlign: "left", mt: 5, mb: 3, mx: "10%", fontWeight: 500 }}
+      >
+        Verify Seller and Product Partnership Agreement
+      </Typography>
+      <Typography variant="body2" sx={{ textAlign: "left", mb: 2, mx: "10%" }}>
+        This policy informs you about verifying sellers, product partners and contract formation. Read carefully.
+      </Typography>
+      <Typography variant="body2" sx={{ textAlign: "left", mb: 2, mx: "10%" }}>
+        I) Verify Seller: Personal subscription services include Matric Olive Premium Access behind paywall content. When you subscribe to Matricolive Premium Access, you get access to opinions, comments and exclusive features selected especially for you by www.matricolive.com editors. <br /> <br />
+        II) Product Partners: In the contract you are informed that we accept investment on the product subject to certain conditions. <br />
+        - You will invest on our product and it will be sold from our site. <br />
+        - A customer can invest up to fifteen amounts. <br />
+        - He will get all the information about product sales and his investment or income on the seller dashboard. <br />
+        - Don't lend or share your password with anyone else to keep your account safe. <br /> <br />
+        III) Type of Subscription: Your subscription type will be determined by Services. As a seller you can accept subscriptions and a partner can accept subscriptions. <br /> <br />
+        IV) Contract information: Matricolive will attempt to process your subscription promptly but does not guarantee that your subscription will be active within any specified time frame. By submitting your payment and other subscription details, you are making an offer to us to purchase a subscription. Your offer will only be accepted by us and a contract will be formed when we have successfully verified your payment details and email address, the quality, commercial conduct and authenticity of your products, at which time we will grant you access to your subscription. Matricolive reserves the right to reject any offer at its sole discretion, for any reason or no reason. <br /> <br />
+        We never allow unsolicited sales. After our representative completes the verification and verification process, you will be given permission to sell the product. And in partnership agreement you will invest in our product and we will sell it through us and give you share of partnership. Metricolive always works for yours. But Metricolive reserves the right to suspend your account for any suspicious activity like spam or illegal.
+      </Typography>
+      <Typography
+        variant="body1"
+        sx={{ textAlign: "left", mt: 5, mb: 3, mx: "10%", fontWeight: 500 }}
+      >
+        Buying Experience
+      </Typography>
+      <Typography variant="body2" sx={{ textAlign: "left", mb: 2, mx: "10%" }}>
+        In this policy, Matricolive informs you about the payment, product warranty, and delivery information. We have tried to make the process of purchasing products through your online easier. Read the trams below: <br /> <br />
+        I) Price and Payment: The fixed price of a product comes from the quality of a product, environmental conditions and the market value of the product. We request you to pay it. We do not accept and do not allow the customer to pay more or less than this. <br /> 
+        Prices are as per our published list price from time to time. Tram reserves the right to change its prices prior to delivery due to changes in market conditions, and to correct inaccuracies or misprints at any time. All prices exclude delivery charges which will be charged at the prevailing rate at the time. All prices and delivery costs exclude VAT. <br /><br />
+        II) Delivery Charges and Process: We deliver through our own agencies and partner agencies. We use door to door delivery system for your convenience. And then comes the delivery charge based on distance to location, product weight, height, and the type of service you want. <br /> <br />
+        Note: If there is any damage to the product, it is requested not to accept the delivery. We never allow our customers to provide substandard products.
+      </Typography>
+      <Typography
+        variant="body1"
+        sx={{ textAlign: "left", mt: 5, mb: 3, mx: "10%", fontWeight: 500 }}
+      >
+        Limitation of Warranty
+      </Typography>
+      <Typography variant="body2" sx={{ textAlign: "left", mb: 2, mx: "10%" }}>
+        All products covered by warranty are accompanied by all warranty documents. The product is made functional within the warranty period. You can contact our 24 hours helpline to inquire about product warranty or any warranty issue.
+      </Typography>
+      <Typography
+        variant="body1"
+        sx={{ textAlign: "left", mt: 5, mb: 3, mx: "10%", fontWeight: 500 }}
+      >
+        Delivery Risk and Product Defect
+      </Typography>
+      <Typography variant="body2" sx={{ textAlign: "left", mb: 2, mx: "10%" }}>
+        We always provide high quality service to our customers. We never provide any defective products to our customers. In case of product defects due to mechanical accident or carelessness, the responsibility of the respective responsible officials. Our customers bear no liability.
+      </Typography>
+      <Typography
+        variant="body1"
+        sx={{ textAlign: "left", mt: 5, mb: 3, mx: "10%", fontWeight: 500 }}
+      >
+        Return Policy
+      </Typography>
+      <Typography variant="body2" sx={{ textAlign: "left", mb: 2, mx: "10%" }}>
+        Any product that doesn't suit you is definitely returnable. The product is returnable within 1 week from the date of purchase. In that case you need to contact us with relevant documents like receipt token and product receipt. Then we will allow it for return.
+      </Typography>
+      <Typography
+        variant="body1"
+        sx={{ textAlign: "left", mt: 5, mb: 3, mx: "10%", fontWeight: 500 }}
+      >
+        Order Cancellation
+      </Typography>
+      <Typography variant="body2" sx={{ textAlign: "left", mb: 2, mx: "10%" }}>
+        Customers may expect to receive genuine, high-quality products from Matricolive. Therefore, after receiving the purchased product from the approved vendors, Matricolive always performs Quality Control inspections. In order to provide our clients with the greatest online shopping experience possible, Matricolive retains the right to reject any purchase placed by a customer if any quality issues are discovered during Quality Control inspections of the ordered goods.
+      </Typography>
+      <Typography
+        variant="body1"
+        sx={{ textAlign: "left", mt: 5, mb: 3, mx: "10%", fontWeight: 500 }}
+      >
+        Pricing Policy
+      </Typography>
+      <Typography variant="body2" sx={{ textAlign: "left", mb: 2, mx: "10%" }}>
+        I) Product price: The price determined by a product depends on its quality, environmental conditions and market value of the product. <br /> <br />
+        II) Subscription Price: It comes as per the vendor's product. We determine this by considering the size, weight, other aspects of the product.<br /> <br />
+        III) Delivery cost: Delivery charges are based on the distance of the place, product weight, height, and the type of service you want. <br /> <br />
+        IV) Pricing error: If we mistakenly tell you a price online or otherwise, we are not obligated to pay you a subscription at that price, even if we mistakenly accept your offer to purchase a subscription at that price, and we reserve the right to later correct any price error. Inform you. If we do this, you can cancel the subscription without any obligation to us and we will refund you the full amount you paid us, or you can pay the correct price. If you refuse to exercise any of these choices, we may cancel your subscription and refund to you the full amount you have paid us. We will always act in good faith in determining whether an actual pricing error has occurred.
+      </Typography>
+      <Typography
+        variant="body1"
+        sx={{ textAlign: "left", mt: 5, mb: 3, mx: "10%", fontWeight: 500 }}
+      >
+        Governing Law
+      </Typography>
+      <Typography variant="body2" sx={{ textAlign: "left", mb: 2, mx: "10%" }}>
+        The laws governing eCommerce in Bangladesh shall govern the interpretation and application of these Terms of Service and any other agreements by which we provide you with Services. A Bangladeshi court with appropriate jurisdiction must hear any and all actionable legal claims or proceedings originating from or related to this website.
+      </Typography>
     </Box>
   );
 };
