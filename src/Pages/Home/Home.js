0091c20636a1331422d4e45dfdb0099e708f02a2
Rakibul-Hasan-max <rakibulhasan4041@gmail.com>
2023-08-09 21:10:23 +0600

    upd4

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,8 +1,6 @@
-import BannerThree from "../Components/BannerThree/BannerThree";
 import Carousel from "../Components/Carousel/Carousel";
 import Dense from "../Components/Dense/Dense";
 import MidBanner from "../Components/MidBanner/MidBanner";
-import SectionFour from "../Components/SectionFour/SectionFour";
 import SectionOne from "../Components/SectionOne/SectionOne";
 import SectionThree from "../Components/SectionThree/SectionThree";
 import SectionTwo from "../Components/SectionTwo/SectionTwo";
@@ -16,8 +14,8 @@ const Home = () => {
       <SectionTwo />
       <MidBanner />
       <SectionThree />
-      <BannerThree />
-      <SectionFour />
+      {/* <BannerThree /> */}
+      {/* <SectionFour /> */}
     </div>
   );
 };
