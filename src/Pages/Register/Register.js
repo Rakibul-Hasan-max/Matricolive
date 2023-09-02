8dab00cf47f646d067509f9722c7cff6b3851f4a
Rakibul-Hasan-max <rakibulhasan4041@gmail.com>
2023-09-02 21:45:14 +0600

    new l22g

--- src/Pages/Register/Register.js
+++ src/Pages/Register/Register.js
@@ -7,22 +7,14 @@ import TextField from "@mui/material/TextField";
 import Typography from "@mui/material/Typography";
 import { AuthContext } from "../../contexts/AuthProvider";
 import { Grid } from '@mui/material';
+import { useForm } from 'react-hook-form';
 
 const Register = () => {
 
-  const { signIn } = useContext(AuthContext);
+  const { register, handleSubmit, formState: { errors } } = useForm();
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const form = event.target;
-    const email = form.email.value;
-    const password = form.password.value;
-    console.log(email, password);
-    signIn(email, password)
-      .then(result => {
-          const user = result.user;
-          console.log(user);
-      })
+  const onSubmit = data => {
+    console.log(data)
   };
 
   return (
@@ -41,12 +33,13 @@ const Register = () => {
           <Typography component="h1" variant="h6" sx={{ pb: 2 }}>
             Create your Matricolive account
           </Typography>
-          <Box component="form" onSubmit={handleSubmit}>
+          <form component="form" onSubmit={handleSubmit(onSubmit)}>
             <Grid container spacing={2}>
               <Grid item xs={12}>
                 <TextField
                   autoComplete="given-name"
                   name="fullName"
+                  {...register("fullName")}
                   required
                   fullWidth
                   id="fullName"
@@ -61,6 +54,7 @@ const Register = () => {
                   id="email"
                   label="Email Address"
                   name="email"
+                  {...register("email")}
                   autoComplete="email"
                   type="email"
                 />
@@ -72,6 +66,7 @@ const Register = () => {
                   id="phone"
                   label="Phone"
                   name="phone"
+                  {...register("phone")}
                   autoComplete="phone"
                   type="number"
                 />
@@ -81,22 +76,14 @@ const Register = () => {
                   required
                   fullWidth
                   name="password"
+                  {...register("password", {minLength: 6,
+                    maxLength: 16,})}
                   label="Password"
                   type="password"
                   id="password"
                   autoComplete="new-password"
                 />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  name="password"
-                  label="Confirm Password"
-                  type="password"
-                  id="password"
-                  autoComplete="new-password"
-                />
+                {errors.password?.type === 'minLength' && <Typography variant="body2">Passwords must be at least 6 characters.</Typography>}
               </Grid>
             </Grid>
             <Button
@@ -119,7 +106,7 @@ const Register = () => {
                 </Link>
               </Typography>
             </Box>
-          </Box>
+          </form>
         </Box>
       </Container>
     </Box>
@@ -127,3 +114,130 @@ const Register = () => {
 };
 
 export default Register;
+
+
+
+
+
+
+
+// import { useContext } from "react";
+// import { Helmet } from "react-helmet-async";
+// import { useForm } from "react-hook-form";
+// import { AuthContext } from "../../providers/AuthProvider";
+// import { Link, useNavigate } from "react-router-dom";
+// import Swal from 'sweetalert2'
+// import SocialLogin from "../Shared/SocialLogin/SocialLogin";
+
+// const SignUp = () => {
+
+//     const { register, handleSubmit, reset, formState: { errors } } = useForm();
+//     const { createUser, updateUserProfile } = useContext(AuthContext);
+//     const navigate = useNavigate();
+
+//     const onSubmit = data => {
+
+//         createUser(data.email, data.password)
+//             .then(result => {
+
+//                 const loggedUser = result.user;
+//                 console.log(loggedUser);
+
+//                 updateUserProfile(data.name, data.photoURL)
+//                     .then(() => {
+//                         const saveUser = { name: data.name, email: data.email }
+//                         fetch('https://bistro-boss-server-fawn.vercel.app/users', {
+//                             method: 'POST',
+//                             headers: {
+//                                 'content-type': 'application/json'
+//                             },
+//                             body: JSON.stringify(saveUser)
+//                         })
+//                             .then(res => res.json())
+//                             .then(data => {
+//                                 if (data.insertedId) {
+//                                     reset();
+//                                     Swal.fire({
+//                                         position: 'top-end',
+//                                         icon: 'success',
+//                                         title: 'User created successfully.',
+//                                         showConfirmButton: false,
+//                                         timer: 1500
+//                                     });
+//                                     navigate('/');
+//                                 }
+//                             })
+
+
+
+//                     })
+//                     .catch(error => console.log(error))
+//             })
+//     };
+
+//     return (
+//         <>
+//             <Helmet>
+//                 <title>Bistro Boss | Sign Up</title>
+//             </Helmet>
+//             <div className="hero min-h-screen bg-base-200">
+//                 <div className="hero-content flex-col lg:flex-row-reverse">
+//                     <div className="text-center lg:text-left">
+//                         <h1 className="text-5xl font-bold">Sign up now!</h1>
+//                         <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+//                     </div>
+//                     <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+//                         <form onSubmit={handleSubmit(onSubmit)} className="card-body">
+//                             <div className="form-control">
+//                                 <label className="label">
+//                                     <span className="label-text">Name</span>
+//                                 </label>
+//                                 <input type="text"  {...register("name", { required: true })} name="name" placeholder="Name" className="input input-bordered" />
+//                                 {errors.name && <span className="text-red-600">Name is required</span>}
+//                             </div>
+//                             <div className="form-control">
+//                                 <label className="label">
+//                                     <span className="label-text">Photo URL</span>
+//                                 </label>
+//                                 <input type="text"  {...register("photoURL", { required: true })} placeholder="Photo URL" className="input input-bordered" />
+//                                 {errors.photoURL && <span className="text-red-600">Photo URL is required</span>}
+//                             </div>
+//                             <div className="form-control">
+//                                 <label className="label">
+//                                     <span className="label-text">Email</span>
+//                                 </label>
+//                                 <input type="email"  {...register("email", { required: true })} name="email" placeholder="email" className="input input-bordered" />
+//                                 {errors.email && <span className="text-red-600">Email is required</span>}
+//                             </div>
+//                             <div className="form-control">
+//                                 <label className="label">
+//                                     <span className="label-text">Password</span>
+//                                 </label>
+//                                 <input type="password"  {...register("password", {
+//                                     required: true,
+//                                     minLength: 6,
+//                                     maxLength: 20,
+//                                     pattern: /(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])(?=.*[a-z])/
+//                                 })} placeholder="password" className="input input-bordered" />
+//                                 {errors.password?.type === 'required' && <p className="text-red-600">Password is required</p>}
+//                                 {errors.password?.type === 'minLength' && <p className="text-red-600">Password must be 6 characters</p>}
+//                                 {errors.password?.type === 'maxLength' && <p className="text-red-600">Password must be less than 20 characters</p>}
+//                                 {errors.password?.type === 'pattern' && <p className="text-red-600">Password must have one Uppercase one lower case, one number and one special character.</p>}
+//                                 <label className="label">
+//                                     <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+//                                 </label>
+//                             </div>
+//                             <div className="form-control mt-6">
+//                                 <input className="btn btn-primary" type="submit" value="Sign Up" />
+//                             </div>
+//                         </form>
+//                         <p><small>Already have an account <Link to="/login">Login</Link></small></p>
+//                         <SocialLogin></SocialLogin>
+//                     </div>
+//                 </div>
+//             </div>
+//         </>
+//     );
+// };
+
+// export default SignUp;
