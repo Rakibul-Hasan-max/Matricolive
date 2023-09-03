54c247ca8a1c4a30cd604ab13620840a03628c59
Rakibul-Hasan-max <rakibulhasan4041@gmail.com>
2023-09-03 10:27:18 +0600

    private set up done

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,107 +1,3 @@
-// import Box from "@mui/material/Box";
-// import Button from "@mui/material/Button";
-// import Container from "@mui/material/Container";
-// import CssBaseline from "@mui/material/CssBaseline";
-// import Link from "@mui/material/Link";
-// import { createTheme, ThemeProvider } from "@mui/material/styles";
-// import TextField from "@mui/material/TextField";
-// import Typography from "@mui/material/Typography";
-// import { useContext } from "react";
-// import { AuthContext } from "../../contexts/AuthProvider";
-
-// const theme = createTheme();
-
-// export default function Login() {
-//   const {signIn} = useContext(AuthContext);
-
-//   const handleSubmit = (event) => {
-//     event.preventDefault();
-//     const data = new FormData(event.currentTarget);
-//     console.log({
-//       email: data.get("email"),
-//       password: data.get("password"),
-//     });
-//     signIn("email", "password")
-//     .then(result => {
-//       const user = result.user;
-//       console.log(user);
-//     })
-//   };
-
-//   return (
-//     <ThemeProvider theme={theme}>
-//       <Container component="main" maxWidth="xs" sx={{ py: "100px" }}>
-//         <CssBaseline />
-//         <Box
-//           sx={{
-//             alignItems: "center",
-//             mt: 1,
-//             backgroundColor: "white",
-//             p: 3,
-//             borderRadius: 2,
-//             filter: "drop-shadow(0 0 0.20rem gray)",
-//           }}
-//         >
-//           <Typography component="h1" variant="h6">
-//             Login
-//           </Typography>
-//           <Box component="form" onSubmit={handleSubmit} noValidate>
-//             <TextField
-//               margin="normal"
-//               required
-//               fullWidth
-//               id="email"
-//               label="Email Address"
-//               name="email"
-//               type="email"
-//               autoComplete="email"
-//               autoFocus
-//             />
-//             <TextField
-//               margin="normal"
-//               required
-//               fullWidth
-//               name="password"
-//               label="Password"
-//               type="password"
-//               id="password"
-//               autoComplete="current-password"
-//             />
-//             <Box style={{ textAlign: "left" }}>
-//               <Link href="#" variant="body2" style={{ textDecoration: "none" }}>
-//                 Forgot password?
-//               </Link>
-//             </Box>
-//             <Button
-//               type="submit"
-//               fullWidth
-//               variant="contained"
-//               sx={{ mt: 3, mb: 2 }}
-//             >
-//               Login
-//             </Button>
-//             <Box>
-//               <Typography variant="body2">
-//                 Don't have an account?
-//                 <Link
-//                   href="register"
-//                   variant="body2"
-//                   style={{ marginLeft: 10, textDecoration: "none" }}
-//                 >
-//                   Register
-//                 </Link>
-//               </Typography>
-//             </Box>
-//           </Box>
-//         </Box>
-//       </Container>
-//     </ThemeProvider>
-//   );
-// }
-
-
-
-
 import React, { useContext } from 'react';
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
@@ -110,10 +6,15 @@ import Link from "@mui/material/Link";
 import TextField from "@mui/material/TextField";
 import Typography from "@mui/material/Typography";
 import { AuthContext } from "../../contexts/AuthProvider";
+import { useLocation, useNavigate } from 'react-router-dom';
 
 const Login = () => {
 
   const { signIn } = useContext(AuthContext);
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const from = location.state?.from?.pathname || "/";
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -125,6 +26,7 @@ const Login = () => {
       .then(result => {
           const user = result.user;
           console.log(user);
+          navigate(from, { replace: true });
       })
   };
 
