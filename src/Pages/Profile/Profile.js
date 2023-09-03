bf9dffb2a1f72de9deb0d0d221363749ac47618c
Rakibul-Hasan-max <rakibulhasan4041@gmail.com>
2023-09-03 12:49:15 +0600

    new pre1

--- src/Pages/Profile/Profile.js
+++ src/Pages/Profile/Profile.js
@@ -1,15 +1,15 @@
 import { Box, Typography } from '@mui/material';
-import { useContext } from 'react';
-import { AuthContext } from '../../contexts/AuthProvider';
+// import { useContext } from 'react';
+// import { AuthContext } from '../../contexts/AuthProvider';
 
 const Profile = () => {
 
     // const {user} = useContext(AuthContext);
 
     return (
-        <Box sx={{m: "100px"}}>
+        <Box sx={{m: "200px"}}>
+            <Typography>Currently this page is under Maintenance. We will be get back very soon.</Typography>
             {/* <Typography variant="body1">{user?.displayName}</Typography> */}
-            hello
         </Box>
     );
 };
